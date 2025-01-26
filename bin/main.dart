aa9bbec6210f46c5ab781440ef57ea50e549f2ea
Lin Xinyang <lxyy@mac.home>
2025-01-25 22:14:24 -0500

    Finish assignment

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,18 @@
-import 'dart:io';
 import 'package:lucky_number/lucky.dart';
 
 void main(List<String> arguments) {
-    var lucky = Lucky();
+    if (arguments.isEmpty) {
+        print('Please provide at least one name');
+        return;
+    }
+
+    for (var name in arguments) {
+        // print('Hi $name');
+
+        String capitalizeName = name[0].toUpperCase() + name.substring(1);
+
+        var lucky = Lucky();
+
+        print('Hello $capitalizeName. Your lucky number is ${lucky.luckyNum}');
+    }
 }

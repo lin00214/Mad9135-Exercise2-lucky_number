199457209a6b692d6463fac4e24170747e16720e
Lin Xinyang <lxyy@mac.home>
2025-01-25 20:37:09 -0500

    Initial commit

---
+++ bin/lucky_number.dart
@@ -0,0 +1,5 @@
+import 'package:lucky_number/lucky_number.dart' as lucky_number;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lucky_number.calculate()}!');
+}
---
+++ test/lucky_number_test.dart
@@ -0,0 +1,8 @@
+import 'package:lucky_number/lucky_number.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
